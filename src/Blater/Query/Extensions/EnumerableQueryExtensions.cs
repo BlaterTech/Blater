9f4b8238588f53e76fc8e4949d4500a73519f205
mariogkds <mariogk@blokods.com.br>
2024-05-21 15:36:29 +0100

    Removing useless stuff

--- src/Blater/Query/Extensions/EnumerableQueryExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Blater.Query.Extensions;
-
-public static class EnumerableQueryExtensions
-{
-    public static bool Contains<T>(this IEnumerable<T> source, IEnumerable<T> input)
-    {
-        return input.All(source.Contains);
-    }
-}
--- src/Blater/Query/Extensions/MemberInfoExtensions.cs
+++ src/Blater/Query/Extensions/MemberInfoExtensions.cs
@@ -18,36 +18,5 @@ namespace Blater.Query.Extensions
                 ? jsonProperty.Name
                 : memberInfo.Name.Camelize();
         }
-
-        public static MethodInfo GetMinOrMaxWithoutSelector<T>(this List<MethodInfo> queryableMethods, string methodName)
-            => queryableMethods.Single(
-                mi => mi.Name == methodName
-                      && mi.GetParameters().Length == 1
-                      && mi.GetParameters()[0].ParameterType.GetGenericArguments()[0] == typeof(T));
-
-        public static MethodInfo GetSumOrAverageWithoutSelector<T>(this List<MethodInfo> queryableMethods, string methodName)
-            => queryableMethods.Single(
-                mi => mi.Name == methodName
-                      && mi.GetParameters().Length == 1
-                      && mi.GetParameters()[0].ParameterType.GetGenericArguments()[0] == typeof(T));
-
-        public static MethodInfo GetSumOrAverageWithSelector<T>(this List<MethodInfo> queryableMethods, string methodName)
-            => queryableMethods.Single(
-                mi => mi.Name == methodName
-                      && mi.GetParameters().Length == 2
-                      && IsSelector<T>(mi.GetParameters()[1].ParameterType));
-
-        public static bool IsExpressionOfFunc(this Type type, int funcGenericArgs = 2)
-            => type.IsGenericType
-               && type.GetGenericTypeDefinition() == typeof(Expression<>)
-               && type.GetGenericArguments()[0].IsGenericType
-               && type.GetGenericArguments()[0].GetGenericArguments().Length == funcGenericArgs;
-
-        public static bool IsSelector<T>(this Type type)
-            => type.IsGenericType
-               && type.GetGenericTypeDefinition() == typeof(Expression<>)
-               && type.GetGenericArguments()[0].IsGenericType
-               && type.GetGenericArguments()[0].GetGenericArguments().Length == 2
-               && type.GetGenericArguments()[0].GetGenericArguments()[1] == typeof(T);
     }
 }
