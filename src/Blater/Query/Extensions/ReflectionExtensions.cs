f18f68fdb4d9f373cd10ae4c2c606f690cf67d45
mariogkds <mariogk@blokods.com.br>
2024-05-24 21:04:54 +0100

    🆕 Added: Expression evaluation and replacement of independent sub-trees, helper methods for type related operations, and extension methods for reflection operations.
    GitMoji: `✨` (Add)
    
    Conventional commit message:
    
    * `Add`: Introduce a new feature, an enhancement, or additional functionality.
    
    Long commit message:
    
    > Adds a new `PartialEvaluator` class for evaluating and replacing independent sub-trees in an expression tree. Also includes helper methods for type related operations and extension methods for reflection operations. The `PartialEvaluator` class provides `Eval` methods for evaluating and replacing sub-trees based on a specified condition. The `TypeHelper` class contains methods for working with types, such as finding the enumerable type of a sequence, getting the element type of a sequence, checking if a type is nullable, and converting a nullable type

---
+++ src/Blater/Query/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+
+namespace Blater.Query.Extensions;
+
+public static class ReflectionExtensions
+{
+    public static object? GetValue(this MemberInfo? member, object? instance)
+    {
+        if (member == null)
+        {
+            return null;
+        }
+        
+        if (instance == null)
+        {
+            return null;
+        }
+        
+        switch (member.MemberType)
+        {
+            case MemberTypes.Property:
+                return ((PropertyInfo)member).GetValue(instance, null);
+            case MemberTypes.Field:
+                return ((FieldInfo)member).GetValue(instance);
+            default:
+                return null;
+        }
+    }
+    
+    public static void SetValue(this MemberInfo member, object instance, object value)
+    {
+        switch (member.MemberType)
+        {
+            case MemberTypes.Property:
+                var pi = (PropertyInfo)member;
+                pi.SetValue(instance, value, null);
+                break;
+            case MemberTypes.Field:
+                var fi = (FieldInfo)member;
+                fi.SetValue(instance, value);
+                break;
+            default:
+                throw new InvalidOperationException();
+        }
+    }
+}
---
+++ src/Blater/Query/Helpers/TypeHelper.cs
@@ -0,0 +1,194 @@
+
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Blater.Query.Helpers
+{
+    /// <summary>
+    /// Type related helper methods
+    /// </summary>
+    public static class TypeHelper
+    {        
+        public static Type? FindIEnumerable(Type? seqType)
+        {
+            if (seqType == null || seqType == typeof(string))
+            {
+                return null;
+            }
+            
+            var elementType = GetElementType(seqType);
+            
+            if (elementType != null && seqType.IsArray)
+            {
+                return typeof(IEnumerable<>).MakeGenericType(elementType);
+            }
+            
+            var info = seqType.GetTypeInfo();
+            if (info.IsGenericType)
+            {
+                foreach (var arg in info.GenericTypeParameters)
+                {
+                    var ienum = typeof(IEnumerable<>).MakeGenericType(arg);
+                    if (ienum.GetTypeInfo().IsAssignableFrom(info))
+                    {
+                        return ienum;
+                    }
+                }
+            }
+            var ifaces = info.ImplementedInterfaces.ToList();
+            if (ifaces.Count != 0)
+            {
+                foreach (var iface in ifaces)
+                {
+                    var ienum = FindIEnumerable(iface);
+                    if (ienum != null)
+                    {
+                        return ienum;
+                    }
+                }
+            }
+            if (info.BaseType != null && info.BaseType != typeof(object))
+            {
+                return FindIEnumerable(info.BaseType);
+            }
+            return null;
+        }
+
+        public static Type GetSequenceType(Type elementType)
+        {
+            return typeof(IEnumerable<>).MakeGenericType(elementType);
+        }
+
+        public static Type? GetElementType(Type? seqType)
+        {
+            var ienum = FindIEnumerable(seqType);
+            if (ienum == null)
+            {
+                return seqType;
+            }
+            
+            return ienum.GetTypeInfo().GenericTypeParameters[0];
+        }
+
+        public static bool IsNullableType(Type? type)
+        {
+            return type != null && type.GetTypeInfo().IsGenericType &&
+                   type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        public static bool IsNullAssignable(Type? type)
+        {
+            return type != null && (!type.GetTypeInfo().IsValueType || IsNullableType(type));
+        }
+
+        public static Type? GetNonNullableType(Type? type)
+        {
+            if (IsNullableType(type) && type != null)
+            {
+                return type.GetTypeInfo().GenericTypeParameters[0];
+            }
+            return type;
+        }
+
+        public static Type? GetNullAssignableType(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+            
+            if (!IsNullAssignable(type))
+            {
+                return typeof(Nullable<>).MakeGenericType(type);
+            }
+            return type;
+        }
+
+        public static ConstantExpression GetNullConstant(Type? type)
+        {
+            return Expression.Constant(null, GetNullAssignableType(type) ?? typeof(object));
+        }
+
+        public static Type? GetMemberType(MemberInfo mi)
+        {
+            var fi = mi as FieldInfo;
+            if (fi != null)
+            {
+                return fi.FieldType;
+            }
+            
+            var pi = mi as PropertyInfo;
+            if (pi != null)
+            {
+                return pi.PropertyType;
+            }
+            
+            var ei = mi as EventInfo;
+            if (ei != null)
+            {
+                return ei.EventHandlerType;
+            }
+            
+            var meth = mi as MethodInfo; // property getters really
+            if (meth != null)
+            {
+                return meth.ReturnType;
+            }
+            
+            return null;
+        }
+
+        public static object? GetDefault(Type? type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+            
+            var isNullable = !type.GetTypeInfo().IsValueType || IsNullableType(type);
+            if (!isNullable)
+            {
+                return Activator.CreateInstance(type);
+            }
+            
+            return null;
+        }
+
+        public static bool IsReadOnly(MemberInfo member)
+        {
+
+            switch (member.MemberType)
+            {
+                case MemberTypes.Field:
+                    return (((FieldInfo)member).Attributes & FieldAttributes.InitOnly) != 0;
+                case MemberTypes.Property:
+                    var propertyInfo = (PropertyInfo)member;
+                    return !propertyInfo.CanWrite || propertyInfo.GetSetMethod() == null;
+                default:
+                    return true;
+            }
+        }
+        
+
+        
+        public static bool IsInteger(Type? type)
+        {
+            //TODO does this work?
+            var nnType = GetNonNullableType(type);
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.SByte:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Byte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
