3c9e18021fc615208187dab4e4cbcaa90207abc1
Elerson <elerson@blokods.com.br>
2024-05-24 15:14:00 -0300

    refactor(user-management): rename UserStore to BlaterUserStore and enhance functionality
    - Rename `UserStore` class to `BlaterUserStore` and `UserStore.cs` to `BlaterUserStore.cs`
    - Implement various ASP.NET Core Identity interfaces (`IUserStore`, `IUserClaimStore`, `IUserEmailStore`, etc.) in `BlaterUserStore`
    - Use `ICouchRepository<BlaterUserInternal>` for database interactions in `BlaterUserStore`
    - Simplify constructors in `BlaterResult.cs` and add `Failure` and `HasErrors` properties
    - Update methods in `BlaterAuthHub.cs` to use `UserManager<BlaterUser>` for user management
    - Remove `Models` folder from `Blater.Backend.csproj`
    - Replace `UserStore` with `BlaterUserStore` in `Program.cs` call to `AddUserStore`

--- src/Blater/BlaterResults/BlaterResult.cs
+++ src/Blater/BlaterResults/BlaterResult.cs
@@ -2,25 +2,16 @@ namespace Blater.BlaterResults;
 
 public class BlaterResult
 {
-    public BlaterResult()
-    {
-        Errors = [];   
-    }
-    
-    public BlaterResult(BlaterError error)
-    {
-        Errors = [error];
-    }
+    public BlaterResult() => Errors = [];
 
-    public BlaterResult(IEnumerable<BlaterError> errors)
-    {
-        Errors = errors.ToList();
-    }
+    public BlaterResult(BlaterError error) => Errors = [error];
+
+    public BlaterResult(IEnumerable<BlaterError> errors) => Errors = errors.ToList();
 
     public bool Success { get; set; }
-    
+
     public bool Failure => !Success;
-    
+
     public bool HasErrors => Errors.Count > 0;
 
     public List<BlaterError> Errors { get; set; }
