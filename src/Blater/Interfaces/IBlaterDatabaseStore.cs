e8b0e7bdc85e722a3c94c74a4c216948dbc6a19f
Elerson <elerson@blokods.com.br>
2024-05-27 08:58:53 -0300

    refactor: update parameter name from 'table' to 'typeName'
    Refactored the parameter name in the methods of the `IBlaterDatabaseStore` interface from `table` to `typeName`, affecting `FindOne`, `FindMany`, `Upsert`, and `Count` methods. Updated routing parameters in the `BlaterDatabaseEndpoint` class to match these changes, affecting `GET`, `POST`, and `DELETE` routes. Also, the `BlaterDatabaseHub` class now uses the new `typeName` parameter instead of `table` when calling methods on the `blaterDatabaseStore` object. Changes reflected in `IBlaterDatabaseStore.cs`, `BlaterDatabaseEndpoint.cs`, and `BlaterDatabaseHub.cs` files.

--- src/Blater/Interfaces/IBlaterDatabaseStore.cs
+++ src/Blater/Interfaces/IBlaterDatabaseStore.cs
@@ -4,15 +4,15 @@ namespace Blater.Interfaces;
 
 public interface IBlaterDatabaseStore
 {
-    public Task<BlaterResult<string>> FindOne(string table, Guid id);
-    public Task<BlaterResult<string>> FindOne(string table, string query);
+    public Task<BlaterResult<string>> FindOne(string typeName, Guid id);
+    public Task<BlaterResult<string>> FindOne(string typeName, string query);
 
-    public Task<BlaterResult<IEnumerable<string>>> FindMany(string table, string query);
+    public Task<BlaterResult<IEnumerable<string>>> FindMany(string typeName, string query);
 
-    public Task<BlaterResult> Upsert(string table, string json);
+    public Task<BlaterResult> Upsert(string typeName, string json);
 
     public Task<BlaterResult> DeleteOne(Guid id);
 
-    public Task<BlaterResult<int>> Count(string table);
-    public Task<BlaterResult<int>> Count(string table, string query);
+    public Task<BlaterResult<int>> Count(string typeName);
+    public Task<BlaterResult<int>> Count(string typeName, string query);
 }
