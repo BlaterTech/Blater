67c6a50933d539aa5e58bd24bc46419eecaa07c8
mariogkds <mariogk@blokods.com.br>
2024-05-27 20:44:04 +0100

    ### Commit Message
    ```
    feat: 🎉 add support for JsonPropertyName attribute in NameEvaluator
    
    - Added System.Text.Json.Serialization import to use JsonPropertyNameAttribute.
    - Updated VisitMember method to check for JsonPropertyNameAttribute and use its value as the actual name.
    ```
    
    **Explanation:**
    - **Type (feat)**: Indicates that this commit introduces a new feature.
    - **GitMoji (🎉)**: A celebration emoji to denote a new feature.
    - **Scope**: General change to the `NameEvaluator` class.
    - **Description**: Clearly states the change made and provides a brief overview of the update to the `VisitMember` method to support the `JsonPropertyName` attribute.

--- src/Blater/Query/Transform/Handlers/NameEvaluator.cs
+++ src/Blater/Query/Transform/Handlers/NameEvaluator.cs
@@ -1,92 +1,93 @@
 using System.Collections;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Text.Json.Serialization;
 using Blater.Query.Models;
 
 namespace Blater.Query.Transform.Handlers;
 
-    /// <summary>
-    /// this will extract the name an expression.
-    /// </summary>
-    internal class NameEvaluator : ExpressionVisitor
+/// <summary>
+/// this will extract the name an expression.
+/// </summary>
+internal class NameEvaluator : ExpressionVisitor
+{
+    private readonly VisitorContext? _context;
+    
+    private readonly DynamicDictionary? _right;
+    
+    //private bool _started;
+    public DynamicDictionary? Query;
+    
+    
+    public NameEvaluator(VisitorContext? context, DynamicDictionary? right)
     {
-        private readonly VisitorContext? _context;
-        private readonly DynamicDictionary? _right;
-        //private bool _started;
-        public DynamicDictionary? Query;
-
-
-        public NameEvaluator(VisitorContext? context, DynamicDictionary? right)
+        _context = context;
+        _right = right;
+    }
+    
+    public override Expression? Visit(Expression? exp)
+    {
+        return exp == null ? null : base.Visit(exp);
+    }
+    
+    protected override Expression VisitMethodCall(MethodCallExpression node)
+    {
+        var isDictionary = typeof(IEnumerable).GetTypeInfo().IsAssignableFrom(node.Method.DeclaringType);
+        
+        //only support dictionay index by name
+        if (!isDictionary || (node.Method.Name != "get_Item" && node.Method.Name != "ContainsKey"))
         {
-            _context = context;
-            _right = right;
+            return node;
         }
-
-        public override Expression? Visit(Expression? exp)
+        
+        if (node.Arguments.Count > 1)
         {
-            return exp == null ? null : base.Visit(exp);
+            throw new Exception("sorry get_Item (dictionary indexer) is supported, and there seems to be more than one");
         }
-
-        protected override Expression VisitMethodCall(MethodCallExpression node)
+        
+        if (node.Arguments.FirstOrDefault() is not ConstantExpression arg)
         {
-            var isDictionary = typeof(IEnumerable).GetTypeInfo().IsAssignableFrom(node.Method.DeclaringType);
-
-            //only support dictionay index by name
-            if (!isDictionary || (node.Method.Name != "get_Item" && node.Method.Name != "ContainsKey"))
-            {
-                return node;
-            }
-            
-            if (node.Arguments.Count > 1)
-            {
-                throw new Exception("sorry get_Item (dictionary indexer) is supported, and there seems to be more than one");
-            }
-            
-            if (node.Arguments.FirstOrDefault() is not ConstantExpression arg)
-            {
-                throw new Exception("sorry get_Item (dictionary indexer) is supported, and there seems to be no args");
-            }
-            
-            //Append($"[\"{arg.Value}\"]", false);
-            Append($"{arg.Value}");
-
-            var result = base.VisitMethodCall(node);
-            return result;
+            throw new Exception("sorry get_Item (dictionary indexer) is supported, and there seems to be no args");
         }
-
-        protected override Expression VisitMember(MemberExpression node)
+        
+        //Append($"[\"{arg.Value}\"]", false);
+        Append($"{arg.Value}");
+        
+        var result = base.VisitMethodCall(node);
+        return result;
+    }
+    
+    protected override Expression VisitMember(MemberExpression node)
+    {
+        var type = node.Member.DeclaringType;
+        var name = node.Member.Name;
+        
+        //Check if it has JsonPropertyNameAttribute
+        var jsonPropertyNameAttribute = node.Member.GetCustomAttribute<JsonPropertyNameAttribute>();
+        
+        var actualName = jsonPropertyNameAttribute?.Name ?? name;
+        
+        Append(actualName);
+        
+        var result = base.VisitMember(node);
+        return result;
+    }
+    
+    private void Append(string name)
+    {
+        if (Query == null)
         {
-            var type = node.Member.DeclaringType;
-            var name = node.Member.Name;
-            var actualName = GetActualName(type, name);
-
-            Append(actualName);
-
-            var result = base.VisitMember(node);
-            return result;
-        }
-
-        private void Append(string name)
-        {
-            if (Query == null)
+            Query = new DynamicDictionary
             {
-                Query = new DynamicDictionary
-                {
-                    { name, _right ?? throw new Exception("right is null")}
-                };
-            }
-            else
-            {
-                Query = new DynamicDictionary
-                {
-                    { name, Query }
-                };
-            }
+                { name, _right ?? throw new Exception("right is null") }
+            };
         }
-
-
-        private static string GetActualName(Type? type, string name)
+        else
         {
-            return name;
+            Query = new DynamicDictionary
+            {
+                { name, Query }
+            };
         }
     }
+}
