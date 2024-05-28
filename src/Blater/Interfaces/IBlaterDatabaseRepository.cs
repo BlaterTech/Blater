70f98de151286a48b71adfbd49212da0dc417500
mariogkds <mariogk@blokods.com.br>
2024-05-28 18:21:13 +0100

    ✨ feat(database): refactor methods to use string id and implement partitioned queries
    - Updated `BlaterDatabaseStore` methods to use `string` instead of `Guid` for document IDs.
    - Implemented `FindOne`, `FindMany`, `Upsert`, and `DeleteOne` methods with partitioned query support.
    - Added new `Count` methods for partitioned queries.
    - Updated `CouchDatabaseClient` to include `PutDocument` method for generic documents.
    - Modified interfaces (`ICouchDatabaseClient`, `IBlaterDatabaseStore`, `IBlaterDatabaseRepository`) to reflect changes.
    - Updated endpoints and hubs to align with the new `string` id and partitioned query methods.

--- src/Blater/Interfaces/IBlaterDatabaseRepository.cs
+++ src/Blater/Interfaces/IBlaterDatabaseRepository.cs
@@ -6,13 +6,13 @@ namespace Blater.Interfaces;
 
 public interface IBlaterDatabaseRepository<T>
 {
-    public Task<BlaterResult<T>> FindOne(Guid id);
+    public Task<BlaterResult<T>> FindOne(string id);
     public Task<BlaterResult<T>> FindOne(Expression<Func<T, bool>> predicate);
     public Task<BlaterResult<IReadOnlyList<T>>> FindMany(Expression<Func<T, bool>> predicate);
 
     public Task<BlaterResult> Upsert(T entity);
 
-    public Task<BlaterResult?> DeleteOne(Guid id);
+    public Task<BlaterResult?> DeleteOne(string id);
     public Task<BlaterResult<int>> DeleteMany(Expression<Func<T, bool>> predicate);
 
     public Task<BlaterResult<int>> Count();
--- src/Blater/Interfaces/IBlaterDatabaseStore.cs
+++ src/Blater/Interfaces/IBlaterDatabaseStore.cs
@@ -5,15 +5,15 @@ namespace Blater.Interfaces;
 
 public interface IBlaterDatabaseStore
 {
-    public Task<BlaterResult<string>> FindOne(Guid id);
-    public Task<BlaterResult<string>> FindOne(string typeName, BlaterQuery query);
+    public Task<BlaterResult<string>> FindOne(string id);
+    public Task<BlaterResult<string>> FindOne(string partition, BlaterQuery query);
 
-    public Task<BlaterResult<IEnumerable<string>>> FindMany(string typeName, BlaterQuery query);
+    public Task<BlaterResult<IEnumerable<string>>> FindMany(string partition, BlaterQuery query);
 
-    public Task<BlaterResult> Upsert(string typeName, string json);
+    public Task<BlaterResult<string>> Upsert(string id, string json);
 
-    public Task<BlaterResult?> DeleteOne(Guid id);
+    public Task<BlaterResult?> DeleteOne(string id);
 
-    public Task<BlaterResult<int>> Count(string typeName);
-    public Task<BlaterResult<int>> Count(string typeName, BlaterQuery query);
+    public Task<BlaterResult<int>> Count(string partition);
+    public Task<BlaterResult<int>> Count(string partition, BlaterQuery query);
 }
