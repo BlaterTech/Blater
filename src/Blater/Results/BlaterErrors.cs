d0d6198da5be26cd9cd8e22bc25fdb26e12c01f6
breno <breno@autosystem.top>
2024-06-17 18:40:37 -0300

    ♻️ refactor: consolidate and centralize Blater.Interfaces usings
    Removed redundant `Blater.Interfaces` using statements from multiple files and centralized them in `GlobalUsings.cs`. This refactor improves maintainability and reduces code clutter.
    
    Affected files:
    - `src/Blater.Backend/Endpoints/v1/KeyValue/SetKeyValue.cs`
    - `src/Blater.Backend/Endpoints/v1/Database/Changes/GetChanges.cs`
    - `src/Blater.Backend/Endpoints/v1/KeyValue/DeleteKeyValue.cs`
    - `src/Blater.Backend/Endpoints/v1/KeyValue/GetKeyValue.cs`
    - `src/Blater.Backend/Endpoints/v1/Database/Changes/GetChangesQuery.cs`
    - `src/Blater.Backend/Endpoints/v1/Authentication/ConfirmEmail.cs`
    - `src/Blater.Backend/Endpoints/v1/Authentication/ResetEmail.cs`
    - `

--- src/Blater/Results/BlaterErrors.cs
+++ src/Blater/Results/BlaterErrors.cs
@@ -4,11 +4,27 @@ namespace Blater.Results;
 
 public static class BlaterErrors
 {
+    public static BlaterError Error(string error)
+    {
+        var message = $"Error: {error}";
+        
+        return new BlaterError(message);
+    }
+    
     public static BlaterError Error(string error, HttpStatusCode statusCode)
     {
         var message = $"""
                        "Status: {statusCode}
                        Error: {error}
+                       """;
+        
+        return new BlaterError(message);
+    }
+    
+    public static BlaterError Error(List<BlaterError> errors)
+    {
+        var message = $"""
+                       "Errors: {errors}
                        """;
         
         return new BlaterError(message);
@@ -23,22 +39,8 @@ public static class BlaterErrors
         return new BlaterError(message);
     }
     
-    public static readonly BlaterError GenericInternalError = new("Internal error");
-    public static BlaterError InternalError(string error) => new("Internal error: "                                                  + error);
-    
-    public static BlaterError InternalError(string error, HttpStatusCode statusCode)
-    {
-        var message = $"""
-                      "Status: {statusCode}
-                      Internal error: {error}
-                      """;
-        
-        return new BlaterError(message);
-    }
-    
     public static readonly BlaterError Success = new("Success");
     public static readonly BlaterError NotFound = new("Not found");
-    public static readonly BlaterError GenericBadRequest = new("Bad Request");
     
     public static readonly BlaterError DatabaseError = new("Database error");
     public static readonly BlaterError QueryError = new("Failed to generate query");
