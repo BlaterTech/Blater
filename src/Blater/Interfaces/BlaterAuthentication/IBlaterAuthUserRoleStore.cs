6a18d43dbb4d97694e03d0ffb5d695fd1151fc61
breno <breno@autosystem.top>
2024-06-18 16:17:02 -0300

    ✨ feat(db): add user permission store implementation #123
    Added `BlaterAuthUserPermissionStore` to manage user permissions.
    
    Benefits:
    - Simplifies permission checks
    - Improves code organization
    
    🚧 chore(db): remove permission methods from `BlaterAuthUserRoleStore` #123
    
    Removed permission-related methods from `BlaterAuthUserRoleStore`.
    
    ✨ feat(api): create user permission endpoints #123
    
    Added endpoints for user permission checks and retrieval:
    - `GetUsersInPermissionWithName`
    - `GetPermissions`
    - `GetUsersInPermission`
    - `IsInPermission`
    - `IsInPermissionWithName`
    
    📝 docs(api): document new user permission endpoints #123
    
    Added descriptions and tags for new user permission endpoints in `GroupUserPermission`.
    
    🧹 refactor(api): introduce `BaseUserPermission` for common logic #123
    
    Created abstract `BaseUserPermission` class to streamline endpoint creation.

--- src/Blater/Interfaces/BlaterAuthentication/IBlaterAuthUserRoleStore.cs
+++ src/Blater/Interfaces/BlaterAuthentication/IBlaterAuthUserRoleStore.cs
@@ -19,12 +19,4 @@ public interface IBlaterAuthUserRoleStore
     
     Task<BlaterResult<IReadOnlyList<BlaterUser>>> GetUsersInRole(string roleName);
     Task<BlaterResult<IReadOnlyList<BlaterUser>>> GetUsersInRole(BlaterRole role);
-    
-    Task<BlaterResult<bool>> IsInPermission(string userId, string permissionName);
-    Task<BlaterResult<bool>> IsInPermission(BlaterUser user, BlaterPermission permission);
-    
-    Task<BlaterResult<IReadOnlyList<string>>> GetPermissions(BlaterUser user);
-    
-    Task<BlaterResult<IReadOnlyList<BlaterUser>>> GetUsersInPermission(string permissionName);
-    Task<BlaterResult<IReadOnlyList<BlaterUser>>> GetUsersInPermission(BlaterPermission permission);
 }
