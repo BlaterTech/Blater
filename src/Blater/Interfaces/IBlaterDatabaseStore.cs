95eb3772cd8fbe7649b09ab2ad91015d1848e1fa
mariogkds <mariogk@blokods.com.br>
2024-06-20 12:41:57 +0100

    ✨ feat(db): rename GetChangesQuery to WatchChangesQuery for consistency #1234
    Renamed GetChangesQuery to WatchChangesQuery across the database to better
    reflect its functionality and improve code readability. This change
    ensures method names are consistent and descriptive, aiding future
    maintenance and development.

--- src/Blater/Interfaces/IBlaterDatabaseStore.cs
+++ src/Blater/Interfaces/IBlaterDatabaseStore.cs
@@ -43,7 +43,7 @@ public interface IBlaterDatabaseStore
     /// <param name="partition"></param>
     /// <param name="query"></param>
     /// <returns>Returns the JSON object in string format</returns>
-    IAsyncEnumerable<BlaterResult<string>> GetChangesQuery(string partition, BlaterQuery query);
+    IAsyncEnumerable<BlaterResult<string>> WatchChangesQuery(string partition, BlaterQuery query);
 
     #endregion
 
