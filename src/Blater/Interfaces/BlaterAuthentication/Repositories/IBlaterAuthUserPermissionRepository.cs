63b18b04b882066854e1aa564b4459fa0f92b701
breno <breno@autosystem.top>
2024-06-20 16:55:50 -0300

    ✨ feat(api): update endpoints for user permissions #308
    Revised API endpoints for consistency and clarity:
    - Changed "get-users-in-permission-with-name" to "get-users-in-permission"
    - Changed "is-in-permission-with-name" to "is-in-permission"
    - Updated HTTP verbs for endpoints
    
    Benefits:
    - Improved API readability and consistency
    - Simplified client implementation
    
    🛠️ chore(db): remove unused GetPermissions method
    
    Removed unused GetPermissions method from BlaterAuthUserPermissionRepository
    
    Benefits:
    - Cleaner codebase
    - Reduced potential confusion for developers
    
    🧪 test(db): remove redundant GetPermissions test
    
    Removed test for the deleted GetPermissions method
    
    Benefits:
    - Aligns tests with current codebase
    - Reduces maintenance burden
    
    ✨ feat(sdk): implement updated endpoints in SDK
    
    Implemented new API endpoints in BlaterAuthUserPermissionStoreEndPoints
    
    Benefits:
    - Ensures SDK compatibility with updated backend
    - Provides clearer and more consistent SDK

--- src/Blater/Interfaces/BlaterAuthentication/Repositories/IBlaterAuthUserPermissionRepository.cs
+++ src/Blater/Interfaces/BlaterAuthentication/Repositories/IBlaterAuthUserPermissionRepository.cs
@@ -7,8 +7,6 @@ public interface IBlaterAuthUserPermissionRepository
     Task<bool> IsInPermission(string userId, string permissionName);
     Task<bool> IsInPermission(BlaterUser user, BlaterPermission permission);
     
-    Task<IReadOnlyList<string>> GetPermissions(BlaterUser user);
-    
     Task<IReadOnlyList<BlaterUser>> GetUsersInPermission(string permissionName);
     Task<IReadOnlyList<BlaterUser>> GetUsersInPermission(BlaterPermission permission);
 }
--- src/Blater/Interfaces/BlaterAuthentication/Stores/IBlaterAuthUserPermissionStore.cs
+++ src/Blater/Interfaces/BlaterAuthentication/Stores/IBlaterAuthUserPermissionStore.cs
@@ -8,8 +8,6 @@ public interface IBlaterAuthUserPermissionStore
     Task<BlaterResult<bool>> IsInPermission(string userId, string permissionName);
     Task<BlaterResult<bool>> IsInPermission(BlaterUser user, BlaterPermission permission);
     
-    Task<BlaterResult<IReadOnlyList<string>>> GetPermissions(BlaterUser user);
-    
     Task<BlaterResult<IReadOnlyList<BlaterUser>>> GetUsersInPermission(string permissionName);
     Task<BlaterResult<IReadOnlyList<BlaterUser>>> GetUsersInPermission(BlaterPermission permission);
 }
