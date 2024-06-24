876f070d6605d6818334b3af1d8ab63476efc1d4
mariogkds <mariogk@blokods.com.br>
2024-06-24 19:46:26 +0100

    ✨ refactor(linode): rename endpoints and DTOs for consistency
    Refactored endpoint and DTO classes for consistent naming convention.
    
    - Improved code readability and maintainability.

--- src/Blater/Extensions/UTF8Extensions.cs
+++ src/Blater/Extensions/UTF8Extensions.cs
@@ -2,14 +2,14 @@
 
 namespace Blater.Extensions;
 
-public static class UTF8Extensions
+public static class Utf8Extensions
 {
-    public static string FromUTF8(this byte[] bytes) => Encoding.UTF8.GetString(bytes);
+    public static string FromUtf8(this byte[] bytes) => Encoding.UTF8.GetString(bytes);
     
-    public static string FromUTF8(this ReadOnlyMemory<byte> bytes)
+    public static string FromUtf8(this ReadOnlyMemory<byte> bytes)
     {
         return Encoding.UTF8.GetString(bytes.Span);
     }
     
-    public static byte[] ToUTF8(this string str) => Encoding.UTF8.GetBytes(str);
+    public static byte[] ToUtf8(this string str) => Encoding.UTF8.GetBytes(str);
 }
