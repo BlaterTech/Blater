e01debfa752f276e31ea48c5b0af6fb41d830f47
breno <breno@autosystem.top>
2024-07-05 15:49:49 -0300

    ✨ feat(db): standardize URL format for db client requests (#123)
    Refactored CouchDatabaseClient to standardize URL formats for API calls.
    Removed redundant slashes for cleaner and consistent endpoint requests.
    
    Benefits:
    - Enhances code readability and maintainability.
    - Minimizes potential errors in URL formation.
    
    ---
    
    ✨ feat(ui): add cancellation support to GetChangesQuery endpoint (#456)
    
    Enhanced the GetChangesQuery endpoint by adding support for cancellation tokens. This allows for more responsive UI interactions and better resource management.
    
    ✨ feat(json): add additional JsonConverters for serialization (#789)
    
    Included new JsonConverters to handle string enums and dictionary order direction. This improves JSON serialization/deserialization consistency and handling of special cases.

--- src/Blater/JsonUtilities/JsonExtensions.cs
+++ src/Blater/JsonUtilities/JsonExtensions.cs
@@ -1,7 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
-using Blater.Enumerations;
 
 namespace Blater.JsonUtilities;
 
--- src/Blater/Models/User/BlaterUserToken.cs
+++ src/Blater/Models/User/BlaterUserToken.cs
@@ -1,16 +1,21 @@
-﻿namespace Blater.Models.User;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace Blater.Models.User;
+
+[SuppressMessage("Design", "CA1056:As propriedades do tipo URI não devem ser cadeias de caracteres")]
 public class BlaterUserToken
 {
     public string UserId { get; set; } = null!;
-
+    
     public string Name { get; set; } = null!;
-
+    
     public string Email { get; set; } = null!;
-
+    
+    public string AvatarUrl { get; set; } = null!;
+    
     public bool LockoutEnabled { get; set; }
-
+    
     public List<string> RoleNames { get; set; } = [];
-
+    
     public List<string> Permissions { get; set; } = [];
 }
