d7bd7dcd6fb88e2abf51aeb9b37c2838efa7d846
mariogkds <mariogk@blokods.com.br>
2024-07-10 08:13:54 +0100

    Removing web stuff from Blater

--- src/Blater/Models/Bases/BaseFrontendModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Blater.AutoModelConfigurations;
-using Blater.AutoModelConfigurations.Interfaces;
-
-namespace Blater.Models.Bases;
-
-public abstract class BaseFrontendModel : BaseDataModel, IDataModelConfigurator
-{
-    public abstract void Configure(AutoModelConfigurator configurator);
-}
