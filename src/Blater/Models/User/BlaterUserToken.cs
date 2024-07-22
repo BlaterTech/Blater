6a8bdcd0dbec1bc04fb4e90cbac2835fce78d51f
breno <breno@autosystem.top>
2024-07-22 14:33:32 -0300

    ✨ feat(frontend): add cookie service and integrate with portal #123
    - Added `CookieService` to manage cookies in `Blater.Frontend`.
    - Integrated `CookieService` into `Blater.Portal` for authentication.
    - Updated `Program.cs` to register `CookieService`.
    - Modified `CookieHandler` to include authorization header.
    - Enhanced `Home.razor` to display user details.

--- src/Blater/Models/User/BlaterUserToken.cs
+++ src/Blater/Models/User/BlaterUserToken.cs
@@ -6,16 +6,11 @@ namespace Blater.Models.User;
 public class BlaterUserToken
 {
     public string UserId { get; set; } = null!;
-    
     public string Name { get; set; } = null!;
-    
     public string Email { get; set; } = null!;
-    
     public string AvatarImage { get; set; } = null!;
     
     public bool LockoutEnabled { get; set; }
-    
     public List<string> Roles { get; set; } = [];
-    
     public List<string> Permissions { get; set; } = [];
 }
