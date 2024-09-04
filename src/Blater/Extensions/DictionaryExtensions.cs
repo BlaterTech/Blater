31a23211911fefcd70e3f4f92945c16fa789410d
breno <breno@autosystem.top>
2024-09-04 18:14:48 -0300

    ✨ feat(models): enhance Employee model with validation and configuration #123
    - Added FluentValidation for Employee model
    - Enhanced AutoForm and AutoValidator configurations
    - Improved component properties and configuration handling
    
    This improves form validation and user input handling.

---
+++ src/Blater/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Blater.Extensions;
+
+public static class DictionaryExtensions
+{
+    public static TValue? GetHasFlagValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, Enum @enum)
+        where TKey : Enum
+        where TValue : notnull
+    {
+        var value = dictionary
+                   .FirstOrDefault(x => x.Key.HasFlag(@enum))
+                   .Value;
+
+        return value;
+    }
+}
